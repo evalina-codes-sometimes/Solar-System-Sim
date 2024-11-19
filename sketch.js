997e9ada070d0a6f4ea1a4cf99ac515a4f845029
Evalinge <7005064@spsd.sk.ca>
2024-11-19 13:22:50 -0600

    Began playing with ideas to determine proposal

--- sketch.js
+++ sketch.js
@@ -1,16 +1,40 @@
-// Project Title
-// Your Name
-// Date
+// Capstone Coding Project 
+// Evalina Maille
+// November 19, 2024
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+class Star {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y; 
+    this.colour;
+    this.mass;
+    this.radius;
+    this.surfaceTemp;
+    this.brightness; 
+  }
+  display() {
+    fill(this.colour);
+    stroke("orange");
+    sphere(this.radius); 
+    //rotateY(millis(1000)/36);
+  }
+}
+let sun = new Star;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  angleMode(DEGREES);
+  sun.colour = "yellow"; 
+  sun.radius = 35; 
+
 }
+
 
 function draw() {
   background(220);
-  circle(mouseX, mouseY, 100);
+  orbitControl();
+  sun.display();
 }
