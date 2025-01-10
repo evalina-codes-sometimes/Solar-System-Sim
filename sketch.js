766c41261acc369b60a78d60c63a1e58c4c6ea58
Evalinge <7005064@spsd.sk.ca>
2025-01-10 14:12:24 -0600

    Reverted to working version
    Code taken from December 20 when textures were still working. Will need to update code and reimplement things. :(
    Textures working again, moon orbit is wrong. I would like to add better html and a background that is not just black.

--- sketch.js
+++ sketch.js
@@ -4,28 +4,24 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
 const AU = 35; 
 const EARTH_YEAR = 3650000;
 let planets = [];
 let moons = [];
 let stars = [];
 let stringFlipper = new Map();
-let showOrbits = true;
 let theMoon;
-let sunTexture; let sun;
-let mercuryTexture; let mercury;
-let venusTexture; let venus;
-let ceresTexture; let ceres; 
-let earthTexture; let earth; 
-let jupiterTexture; let jupiter; 
-let saturnTexture; let saturn;
-let uranusTexture; let uranus;
-let neptuneTexture; let neptune; 
-let plutoTexture; let pluto;
-let erisTexture; let eris; 
-
-
+let sunTexture;
+let mercuryTexture;
+let venusTexture;
+let ceresTexture;
+let earthTexture;
+let jupiterTexture;
+let saturnTexture;
+let uranusTexture;
+let neptuneTexture;
+let plutoTexture;
+let erisTexture; 
 class Star {
   constructor(x, y) {
     this.texture;
@@ -37,35 +33,33 @@ class Star {
     this.surfaceTemp;
     this.brightness; 
   }
-
   display() {
     //fill(this.colour);
     noStroke();
     texture(this.texture);
-    sphere(this.diameter/2);
+    sphere(this.diameter/2);//, this.x, this.y); 
+    //rotateY(millis(1000)/36);
   }
  
 }
-
 class Planet {
   constructor(astronomicalUnits, earthYears, orbiting, orbitalInclination){
-    this.mass;
-    this.texture;
-    this.diameter;
-    this.ringSystem;
-    this.moons;
-    this.orbiting = orbiting;
     this.astronomicalUnits = astronomicalUnits;
+    this.orbiting = orbiting;
     this.radians = 0;
-    this.rotationalAxis = 0;
     this.distanceFromSun = this.astronomicalUnits * AU + this.orbiting.diameter/2;
     this.x = Math.cos(this.radians)*this.distanceFromSun;
     this.y = Math.sin(this.radians)*this.distanceFromSun; 
+    this.mass;
+    this.diameter;
     this.orbitalPeriod = earthYears * EARTH_YEAR;
     this.orbitalVelocity = 2* Math.PI * this.distanceFromSun/this.orbitalPeriod;
+    this.moons;
+    this.ringSystem;
     this.eclipticAngle = createVector(0, 0, orbitalInclination);
+    this.rotationalAxis = 0;
+    this.texture;
   }
-
   orbit(){
     this.radians += this.orbitalVelocity; 
     this.x = Math.cos(this.radians*this.orbitalPeriod)*this.distanceFromSun;
@@ -74,41 +68,36 @@ class Planet {
   }
   display(){
     // rotate(this.rotationalAxis, this.eclipticAngle);
-    // rotateZ(this.rotationalAxis);
+    rotateZ(this.rotationalAxis);
     noStroke();
     fill(this.colour);
     push();
     translate(this.x, this.y, 0);
-    
-    // if (this.texture !== undefined){
-    //   rotateX(90);
-    //   texture(this.texture);
-    // }
-    
-    texture(this.texture);
+    // textureMode(IMAGE);
+    if (this.texture !== undefined){
+      rotateX(90);
+      texture(this.texture);
+    }
     sphere(this.diameter/2);
-    //rotate(this.eclipticAngle, [1, 0, 1]);
     this.orbit(this.x, this.y);
     pop();
     //circle(this.x, this.y, this.diameter);
     //translate(this.x, this.y, 0);
-
   }
   displayOrbit() {
     //rotateZ(this.eclipticAngle);
-    //rotate(this.eclipticAngle, [0, 1, 1]);
-    //rotate(this.eclipticAngle, [1, 0, 1]);
+    rotateZ(this.rotationalAxis);
+    // rotate(this.eclipticAngle, [0, 1, 1]);
+    rotate(this.eclipticAngle, [1, 0, 1]);
     noFill();
     strokeWeight(0.5);
     stroke('white');
     circle(this.orbiting.x, this.orbiting.y, this.distanceFromSun*2);
-    
   }
   createMoon(){
-    let someMoon = new Moon(this, 5); 
+    let someMoon = new Moon(this); 
   }
 }
-
 class Moon {
   constructor(orbiting, earthYears){
     
@@ -125,9 +114,10 @@ class Moon {
     this.orbitalVelocity = 2* Math.PI * this.distanceFromOrbiting/this.orbitalPeriod;
   }
   orbit(){
+    rotate(this.orbiting.eclipticAngle, [1, 0, 1]);
     this.radians += this.orbitalVelocity; 
-    this.x = Math.cos(this.orbiting.radians*this.orbiting.orbitalPeriod)*this.orbiting.distanceFromSun + Math.cos(this.radians*this.orbitalPeriod)*this.distanceFromOrbiting;
-    this.y = Math.sin(this.orbiting.radians*this.orbiting.orbitalPeriod)*this.orbiting.distanceFromSun + Math.sin(this.radians*this.orbitalPeriod)*this.distanceFromOrbiting;
+    this.x = Math.cos(this.orbiting.radians*this.orbiting.orbitalPeriod)*this.orbiting.distanceFromSun + Math.cos(this.radians*Math.PI/this.orbitalPeriod)*this.distanceFromOrbiting;
+    this.y = Math.sin(this.orbiting.radians*this.orbiting.orbitalPeriod)*this.orbiting.distanceFromSun + Math.sin(this.radians*Math.PI/this.orbitalPeriod)*this.distanceFromOrbiting;
     //This has an example of how to solve, with a very good formula. Should try it out. 
     //https://www.google.com/search?sca_esv=6998cc71aa68975d&q=sin(x)+%2B+sin(10x)&source=lnms&fbs=AEQNm0Aa4sjWe7Rqy32pFwRj0UkWd8nbOJfsBGGB5IQQO6L3J7pRxUp2pI1mXV9fBsfh39Jw_Y7pXPv6W9UjIXzt09-YtiqJSnyznYMycaNNv7N_qyqA4nWiNpMBQ-7f5KgNVAh12h29aAKQPuzuPcMwfTQBzc1pQOFZyAaBYXuqtpmZcvsjt3wMGURkScfI-cTqUyiiBliztZhwmvMvmJMMIg2jPaA72A&sa=X&ved=2ahUKEwi21-nMwqCKAxVCATQIHer_CzsQ0pQJegQICBAB&biw=1592&bih=776&dpr=1
   }
@@ -152,7 +142,6 @@ class Moon {
     circle(this.orbiting.x, this.orbiting.y, this.distanceFromOrbiting*2);
   }
 }
-
 function checkForStar(body){
   if (stars.includes(sun)){
     return sun;
@@ -167,100 +156,81 @@ function checkForStar(body){
 //create a function to store my map settings to shorten setup 
 
 function assignData(bodiesData){
-  console.log("hello");
-  //n this will work!!!!
+//n this will work!!!!
   //eval(`${bodiesData.getString(0, "theName")} = new ${stringFlipper.get(bodiesData.get(0, "type"))}();`);
   for (let row = 0; row<bodiesData.getRowCount(); row++){
-    // console.log(row);
     let tempThing;
     if (bodiesData.get(row, "type") === "Star"){
-      // console.log(`${bodiesData.get(row, "theName")} = new ${stringFlipper.get(bodiesData.get(row, "type"))}(0,0);`);
       tempThing = eval(`${bodiesData.get(row, "theName")} = new ${stringFlipper.get(bodiesData.get(row, "type"))}(0,0);`);
       tempThing.diameter = bodiesData.get(row, "diamteter"); 
       tempThing.colour = bodiesData.get(row, "colour"); 
-      // stars.push(tempThing);
-      console.log("star made");
+      stars.push(tempThing);
     }
     else if (bodiesData.get(row, 'type') === "Planet"){  //      class Type                                        dist AU,                         Earth Years,                       orbiting object
-      console.log(`${bodiesData.get(row, "theName")} = new ${stringFlipper.get(bodiesData.get(row, "type"))}(bodiesData.getNum(row, "distAu"), bodiesData.getNum(row, "earthYr"), checkForStar());`);
       tempThing = eval(`${bodiesData.get(row, "theName")} = new ${stringFlipper.get(bodiesData.get(row, "type"))}(bodiesData.getNum(row, "distAu"), bodiesData.getNum(row, "earthYr"), checkForStar());`); 
       tempThing.diameter = bodiesData.get(row, "diameter");
       tempThing.colour = bodiesData.get(row, "colour");
       tempThing.eclipticAngle = bodiesData.getNum(row, "orbitalInclination");
-      // planets.push(tempThing);
+      planets.push(tempThing);
 
     }
     tempThing.texture = stringFlipper.get(bodiesData.getString(row, "theTexture"));
     tempThing.diameter = bodiesData.getNum(row, "diameter"); 
     tempThing.orbitalPeriod = bodiesData.getNum(row, "earthYr");
     tempThing.colour = bodiesData.get(row, "colour");
-    if (bodiesData.get(row, 'type') === "Planet"){ 
-      planets.push(tempThing);
-    }
-    if (bodiesData.get(row, 'type') === "Star"){ 
-      stars.push(tempThing);
-    }
   }
 }
-
 function preload(){
   bodiesData = loadTable("SSDataSheet.csv", "csv", "header");
-  sunTexture = loadImage('sunTexture.jpg');
-  mercuryTexture = loadImage('mercuryTexture.jpg');
-  venusTexture = loadImage('venusTexture.jpg');
-  earthTexture = loadImage("no_ice_clouds.jpg");
-  ceresTexture = loadImage('ceresTexture.jpg');
-  jupiterTexture = loadImage('jupiterTexture.jpg');
-  saturnTexture = loadImage('saturnTexture.jpg');
-  uranusTexture = loadImage('uranusTexture.jpg');
-  neptuneTexture = loadImage('neptuneTexture.jpg');
-  plutoTexture = loadImage('plutoTexture.jpg');
-  erisTexture = loadImage('erisTexture.jpg');
+  sunTexture = loadImage('sunTexture.png');
+  mercuryTexture = loadImage('mercuryTexture.png');
+  venusTexture = loadImage('venusTexture.png');
+  earthTexture = loadImage("earthTexture.png");
+  ceresTexture = loadImage('ceresTexture.png');
+  jupiterTexture = loadImage('jupiterTexture.png');
+  saturnTexture = loadImage('saturnTexture.png');
+  uranusTexture = loadImage('uranusTexture.png');
+  neptuneTexture = loadImage('neptuneTexture.png');
+  plutoTexture = loadImage('plutoTexture.png');
+  erisTexture = loadImage('erisTexture.png');
 }
-
 function FlipStrings(){
   stringFlipper.set('Star', Star);
   stringFlipper.set('Planet', Planet);
-  stringFlipper.set('sunTexture', sunTexture); stringFlipper.set("sun", sun);
-  stringFlipper.set('mercuryTexture', mercuryTexture); stringFlipper.set("mercury", mercury);
-  stringFlipper.set('venusTexture', venusTexture); stringFlipper.set('venus', venus);
-  stringFlipper.set('ceresTexture', ceresTexture); stringFlipper.set('ceres', ceres);
-  stringFlipper.set('earthTexture', earthTexture); stringFlipper.set('earth', earth);
-  stringFlipper.set('jupiterTexture', jupiterTexture); stringFlipper.set('jupiter', jupiter);
-  stringFlipper.set('saturnTexture', saturnTexture); stringFlipper.set('saturn', saturn);
-  stringFlipper.set('uranusTexture', uranusTexture); stringFlipper.set('uranus', uranus);
-  stringFlipper.set('neptuneTexture', neptuneTexture); stringFlipper.set('neptune'. neptune);
-  stringFlipper.set('plutoTexture', plutoTexture); stringFlipper.set('pluto', pluto);
-  stringFlipper.set('erisTexture', erisTexture); stringFlipper.set('eris', eris);
+  stringFlipper.set('sunTexture', sunTexture);
+  stringFlipper.set('mercuryTexture', mercuryTexture);
+  stringFlipper.set('venusTexture', venusTexture);
+  stringFlipper.set('ceresTexture', ceresTexture);
+  stringFlipper.set('earthTexture', earthTexture);
+  stringFlipper.set('jupiterTexture', jupiterTexture);
+  stringFlipper.set('saturnTexture', saturnTexture);
+  stringFlipper.set('uranusTexture', uranusTexture);
+  stringFlipper.set('neptuneTexture', neptuneTexture);
+  stringFlipper.set('plutoTexture', plutoTexture);
+  stringFlipper.set('erisTexture', erisTexture);
 }
-
 function setup() {
   FlipStrings();
   createCanvas(windowWidth, windowHeight, WEBGL);
-  debugMode(GRID);
   assignData(bodiesData);
-  // theMoon = new Moon(earth, 27/365);
+  theMoon = new Moon(earth, 27/365);
 }
 
 function draw() {
   background(0);
   orbitControl();
   for (let thePlanet of planets){
+    thePlanet.displayOrbit();
     thePlanet.display();
-    if (showOrbits){
-      thePlanet.displayOrbit();
-    }
   }
   for (let theStar of stars){
     theStar.display();
   }
-  // theMoon.display();
+  theMoon.display();
 }
-
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
-
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
@@ -274,7 +244,3 @@ window.onclick = function(event) {
     }
   }
 };
-
-// function toggleOrbitLines(){
-//   showOrbits = !showOrbits;
-// }
